bfffcf1f6d8539ad435fce292939db0c1dc9aaaf
Enes Ceylan <contact@enesceylan.dev>
2024-12-22 22:45:28 +0300

    Riced some application.

--- sh.nix
+++ sh.nix
@@ -4,6 +4,22 @@
 {
 	programs.zsh = {
 		enable = true; 
+		enableCompletion = true;
+		autosuggestion.enable = true;
+		syntaxHighlighting.enable = true;
+
+		history = {
+			size = 10000;
+			path = "${config.xdg.dataHome}/zsh/history";
+		};
+		oh-my-zsh = {
+			enable = true;
+			theme = "fishy";
+			plugins = [
+				"sudo"
+				"git"
+			];
+		};
 	};
 
 }
